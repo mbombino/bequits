32464e05f713a71fc8076968b20d469a23cd9307
Jabulani Matomela <matomelajabulani@gmail.com>
2024-08-25 23:38:08 +0200

    Made code cleanup

--- src/components/invoice-details.component.js
+++ src/components/invoice-details.component.js
@@ -5,6 +5,7 @@ import { Dayjs } from "dayjs";
 import HeadingSection from "./invoice-heading.component";
 import ItemsSections from "./invoice-items.component";
 import PaymentSection from "./invoice-payment.component";
+import NotesSection from "./invoice-notes.component";
 
 export default function InvoiceDetails() {
   return (
@@ -12,6 +13,7 @@ export default function InvoiceDetails() {
       <HeadingSection />
       <ItemsSections />
       <PaymentSection />
+      <NotesSection />
     </>
   );
 }
--- src/components/invoice-notes.component.js
+++ src/components/invoice-notes.component.js
@@ -1,9 +1,33 @@
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 
 export default function NotesSection() {
   return (
-    <Box display={"flex"}>
-      <Typography>Other notes and instructions</Typography>
-    </Box>
+    <>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        mt={5}
+        ml={5}
+        mr={5}
+        mb={1}
+      >
+        <Typography>Other notes and instructions</Typography>
+        <Typography>*Optional</Typography>
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { ml: 5, mb: 5, width: "85%" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          multiline={true}
+          rows={5}
+          defaultValue={"Thank you for your business!"}
+        ></TextField>
+      </Box>
+    </>
   );
 }
