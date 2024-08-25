d1456470bac6e7f381743102d53eba7aa4116881
Jabulani Matomela <matomelajabulani@gmail.com>
2024-08-25 22:43:11 +0200

    Added an invoice memo page

---
+++ src/components/invoice-notes.component.js
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function NotesSection() {
+  return (
+    <Box display={"flex"}>
+      <Typography>Other notes and instructions</Typography>
+    </Box>
+  );
+}
