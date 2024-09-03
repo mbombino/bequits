5c9d739ba5e244a7f4e994b8a8d18f80df268226
Jabulani Matomela <matomelajabulani@gmail.com>
2024-09-03 14:16:34 +0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-import BillDetails from "./components/BillDetails";
-import ItemList from "./components/ItemList";
-import TotalAmount from "./components/TotalAmount";
 import { Grid, Paper, Box, Button } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -15,20 +12,6 @@ import PDFPreview from "./components/pdf-preview.component";
 import InvoiceDetails from "./components/invoice-details.component";
 
 function App() {
-  const [items, setItems] = React.useState([]);
-
-  const handleAddItem = (item) => {
-    setItems([...items, item]);
-  };
-  const handleDeleteItem = (index) => {
-    const updatedItems = [...items];
-    updatedItems.splice(index, 1);
-    setItems(updatedItems);
-  };
-  const calculateTotalAmount = () => {
-    return items.reduce((total, item) => total + item.quantity * item.price, 0);
-  };
-
   return (
     <Box>
       <Grid container spacing={0}>
